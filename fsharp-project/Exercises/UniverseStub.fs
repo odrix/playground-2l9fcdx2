1ae9dff3e93f4225688eb78327f5f9dd73923d8b
odrix <adribey@free.fr>
2020-05-05 22:45:55 +0200

    essai 1

--- fsharp-project/Exercises/UniverseStub.fs
+++ fsharp-project/Exercises/UniverseStub.fs
@@ -1,4 +1,4 @@
 namespace Answer
     module Exercise1 =
         let CountAllStars (galaxies: int list) =
-            0 // Fix me ! Should return the sum of galaxies
+             List.fold (+) 0 galaxies // Fix me ! Should return the sum of galaxies
